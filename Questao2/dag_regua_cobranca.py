06b3993ffac47019e7a0adf8c08a9c24151ff5c6
Aloisio Filho <aloisiossf@gmail.com>
2025-12-01 12:36:51 -0300

    Criação função para verificar se o arquivo está na pasta

--- Questao2/dag_regua_cobranca.py
+++ Questao2/dag_regua_cobranca.py
@@ -5,3 +5,16 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.utils.dates import days_ago
 from airflow.exceptions import AirflowFailException
+
+# verificar a chegada do arquivo 
+
+def verificar_arquivo(**context):
+    diretorio = '/path/do/diretorio/'  # Substituir pelo caminho real
+    arquivo = 'pagamentos_d-1.csv'
+    caminho_arquivo = os.path.join(diretorio, arquivo)
+
+    # Verifica se o arquivo está no diretório
+    if os.path.exists(caminho_arquivo):
+        context['ti'].xcom_push(key='arquivo_disponivel', value=True)
+    else:
+        raise AirflowFailException(f"Arquivo {arquivo} não encontrado!")
